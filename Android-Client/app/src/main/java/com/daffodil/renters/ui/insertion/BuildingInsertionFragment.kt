d87db4af527657c000503116b8cd790bec8b5cbd
Tanmay Singal <tanmaysingal@icloud.com>
2020-02-23 11:32:42 +0530

    Property Insertion Fragment

--- Android-Client/app/src/main/java/com/daffodil/renters/ui/insertion/BuildingInsertionFragment.kt
+++ Android-Client/app/src/main/java/com/daffodil/renters/ui/insertion/BuildingInsertionFragment.kt
@@ -7,15 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.daffodil.renters.R
-import kotlinx.android.synthetic.main.fragment_building_creation.*
+import kotlinx.android.synthetic.main.fragment_building_insertion.*
 
-class BuildingCreationFragment : Fragment() {
+class BuildingInsertionFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_building_creation, container, false)
+        return inflater.inflate(R.layout.fragment_building_insertion, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
